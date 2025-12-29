cc38d6297f09c8ec13b584ebcd8566869bb71700
EmzdZiddan <emzdziddan@gmail.com>
2025-12-29 14:43:08 +0700

    awd

---
+++ check.js
@@ -0,0 +1,81 @@
+// // WHILE, FOR
+
+// let a = 1;
+
+// while (a <= 5) {
+//     console.log ('Oke' + a);
+//     a++
+// }
+
+// // 
+
+// for (let a = 1; a <= 5; a++) {
+//     console.log('Oke'+a)
+// }
+
+
+// // IF ELSE
+// let a = prompt('Masukkan Angka')
+
+// if (a&2 == 0) {
+//     alert('Genap')
+// } else {
+//     alert('Ganjil')
+// }
+
+// // FUNCTION
+// // function declaration
+
+// function tambah (a,b) {
+//     return console.log(4+4)
+// };
+
+// tambah();
+
+// // function expression
+// let a = function () {
+//     return (console.log(4+4))
+// };
+
+// a();
+
+// // arrow function
+// let a = () => {
+//     return (console.log(4+4))
+// };
+
+// a();
+
+// // OBJECT
+// // objek literal 
+
+// let penduduk = {
+//     nama : 'lala',
+//     umur : 22
+// }
+
+// // objek function
+
+// function penduduk2(nama, umur, nik) {
+//     let pen = {};
+//     pen.nama = nama;
+//     pen.umur = umur;
+//     pen.nik = nik;
+
+//     return pen;
+// }
+
+// let orang2 =penduduk2('popo', 22, 1222);
+// console.table(orang2); 
+
+// // objek constructor
+// function penduduk3 (nama, umur, nik) {
+//     this.nama = nama;
+//     this.umur = umur;
+//     this.nik = nik; 
+// }
+
+// let entry = new penduduk3('lalapo',22,1110)
+// console.table(entry)
+
+
