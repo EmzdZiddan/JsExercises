bf8783b4235e341168dbe9cc097fadd6f2c0aaf1
EmzdZiddan <emzdziddan@gmail.com>
2026-01-02 01:10:12 +0700

    ww

--- tes.js
+++ tes.js
@@ -1,6 +1,22 @@
-let pBaru = document.createElement('p');
+const pBaru = document.createElement('p');
 const isiTeks = document.createTextNode('Paragraf Anayar');
 pBaru.appendChild(isiTeks);
 
 const cek = document.getElementById('a');
 cek.appendChild(pBaru);
+
+
+/////////////////////////////////////////////////////////////
+
+const liBaru = document.createElement('li');
+const isiLi = document.createTextNode('Item Baru woy');
+liBaru.appendChild(isiLi);
+
+const ulBaru = document.querySelector('section#b ul');
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+ulBaru.insertBefore(liBaru, li2);
+
+
+
+
+
