c3123639f9f9160b2fe87a7632a79b15992a5039
EmzdZiddan <emzdziddan@gmail.com>
2026-01-02 01:57:57 +0700

    oke

--- tes.js
+++ tes.js
@@ -12,11 +12,25 @@ const liBaru = document.createElement('li');
 const isiLi = document.createTextNode('Item Baru woy');
 liBaru.appendChild(isiLi);
 
-const ulBaru = document.querySelector('section#b ul');
 const li2 = document.querySelector('section#b ul li:nth-child(2)');
-ulBaru.insertBefore(liBaru, li2);
+li2.before(liBaru);
 
 
+/////////////////////////////////////////////////////////////
 
+const tujuan = document.getElementsByTagName('a')[0];
+tujuan.remove();
+
+/////////////////////////////////////////////////////////////
+
+const sb = document.querySelector('section#b');
+const p4 = document.querySelector('section#b p');
+
+const h2Baru = document.createElement('h2');
+h2Baru.textContent = 'Judul Baru!';
+
+sb.replaceChild(h2Baru, p4);
+
+/////////////////////////////////////////////////////////////
 
 
