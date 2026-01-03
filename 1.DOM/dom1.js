d0822fff9da8196d7902e6fefd077fb26b2842e0
EmzdZiddan <emzdziddan@gmail.com>
2026-01-04 03:34:18 +0700

    update dom1.js with efficient syntax

--- 1.DOM/dom1.js
+++ 1.DOM/dom1.js
@@ -1,33 +1,28 @@
 // MENAMBAHKAN ELEMENT 1
 const pBaru = document.createElement('p');
-const isiTeks = document.createTextNode('Paragraf Anayar');
-pBaru.appendChild(isiTeks);
+pBaru.textContent = 'ini paragraf anyarrr'
 
 const cek = document.getElementById('a');
-cek.appendChild(pBaru);
-
+cek.append(pBaru);
 
 // MENAMBAHKAN ELEMENT 2
 const liBaru = document.createElement('li');
-const isiLi = document.createTextNode('Item Baru woy');
-liBaru.appendChild(isiLi);
+liBaru.textContent = ('Item Baru woy');
 
 const li2 = document.querySelector('section#b ul li:nth-child(2)');
-li2.before(liBaru);
+li2.after(liBaru);
 
 
-// MENGHAPUS ELEMENT
-const tujuanPar = document.getElementById('a');
+// MENGHAPUS ELEMENT 1
 const tujuan = document.getElementsByTagName('a')[0];
-tujuanPar.removeChild(tujuan);
 
+tujuan.remove();
 
 // MENGGANTI ELELMEN 
-const sb = document.querySelector('section#b');
 const p4 = document.querySelector('section#b p');
 
 const h2Baru = document.createElement('h2');
 h2Baru.textContent = 'Judul Baru!';
 
-sb.replaceChild(h2Baru, p4);
+p4.replaceWith(h2Baru);
 
--- 1.DOM/dom2.js
+++ 1.DOM/dom2.js
@@ -1,16 +1,23 @@
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('mouseenter', () => {
-    const ul = document.querySelector('section#b ul');
-    const li = document.createElement('li');
-    li.textContent = 'BISA';
-    ul.appendChild(li);
-} ); 
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', () => {
+//     const ul = document.querySelector('section#b ul');
+//     const li = document.createElement('li');
+//     li.textContent = 'BISA';
+//     ul.appendChild(li);
+// } ); 
 
 
-const p3 = document.querySelector('.p3');
-p3.onclick = () => {
-    p3.style.backgroundColor = 'lightblue';
+// const p3 = document.querySelector('.p3');
+// p3.onclick = () => {
+//     p3.style.backgroundColor = 'lightblue';
   
-}
+// }
 
 
+
+const p4 = document.querySelector('section#b p');
+
+const h2Baru = document.createElement('h2');
+h2Baru.textContent = 'Judul Baru!';
+
+p4.replaceWith(h2Baru);
